d1d3c0e8be220431c5fe812cd52090f1fae9e286
Rami <138556019+rami242424@users.noreply.github.com>
2023-12-15 16:10:31 +0900

    moveTo(0,0)를 미리 정의 함으로써 첫 시작 선을 0,0위치에서 시작된다

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ canvas.width = 800;
 canvas.height = 800;
 
 ctx.lineWidth = 2;
+ctx.moveTo(0, 0); // 선이 모서리에서 시작되도록 하기
 
 function onClick(event){
-    // console.log(event); // 콘솔로 클릭한 좌표값은 offsetX,Y라는 것을 알 수 있다.
     ctx.lineTo(event.offsetX, event.offsetY);
     ctx.stroke();
 }
