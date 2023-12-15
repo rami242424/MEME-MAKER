26a4927c25250d17cb4dc23f26392654134a0639
Rami <138556019+rami242424@users.noreply.github.com>
2023-12-15 16:28:22 +0900

    그릴때 마다 모든 선의 경로를 바꿔줌으로써 모두 다른색으로 그려지게만들기

--- app.js
+++ app.js
@@ -4,13 +4,28 @@ const ctx = canvas.getContext("2d");
 
 canvas.width = 800;
 canvas.height = 800;
-
 ctx.lineWidth = 2;
 
+// 색을 array로 만들어보자(움직일때마다, 새로운 선을 그릴떄 마다 색이 달라지게)
+const colors = [
+    "#ff3838",
+    "#ffb8b8",
+    "#c56cf0",
+    "#ff9f1a",
+    "#fff200",
+    "#32ff7e",
+    "#7efff5",
+    "#18dcff",
+    "#7d5fff",
+]
+
 function onClick(event){
+    ctx.beginPath(); // 모든 선의 경로를 매번 바꿔주기
     ctx.moveTo(0, 0);
+    const color = colors[Math.floor(Math.random() * colors.length)]
+    ctx.strokeStyle = color;
     ctx.lineTo(event.offsetX, event.offsetY);
     ctx.stroke();
 }
 
-canvas.addEventListener("mousemove", onClick); // click > mousemove로 바꿈으로써 클릭이 아닌 마우스를 움직이므로써 그림이 그려지게 한다.
+canvas.addEventListener("mousemove", onClick); 
