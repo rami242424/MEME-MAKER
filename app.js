d1e832111a0baa5026a83553d13639f8fc0d062f
Rami <138556019+rami242424@users.noreply.github.com>
2023-12-19 13:34:25 +0900

    Destroy 버튼 및 기능 추가, ctx.fillStyle = white;

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const modeBtn = document.getElementById("mode-btn");
+const destroyBtn = document.getElementById("destroy-btn");
 const colorOptions = Array.from(
     document.getElementsByClassName("color-option")
 );
@@ -7,8 +8,14 @@ const lineWidth =  document.getElementById('line-width');
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
-canvas.width = 800;
-canvas.height = 800;
+// ctx.fillRect(0, 0, 800, 800); 중복으로 많이 쓰이니 상수로 만들자
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
+
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+// canvas.width = 800;
+// canvas.height = 800;
 ctx.lineWidth = lineWidth.value; 
 
 let isPainting = false; 
@@ -37,9 +44,6 @@ function onLineWidthChange(event){
 }
 
 function onColorChange(event){
-    // console.log(event.target.value); // 선택한 색상이 콘솔에 잘 뜨는지 확인
-    // fillColor : 사각형을 만들면, 그 안을 채워주는 색상
-    // strokeColor : 라인(선)에서 쓰임
     ctx.strokeStyle = event.target.value;
     ctx.fillStyle = event.target.value;
 }
@@ -61,10 +65,18 @@ function onModeClick(){
     }
 }
 
+// Fill 동작
 function onCanvasClick(){
     if(isFilling){
-        ctx.fillRect(0, 0, 800, 800);
+        // ctx.fillRect(0, 0, 800, 800);
+        ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     }
+}
+
+// 초기화 동작
+function OnDestroyClick(){
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 }
 
 canvas.addEventListener("mousemove", onMove);
@@ -80,4 +92,5 @@ color.addEventListener("change", onColorChange);
 // 컬러를 클릭할떄마다 호출 될 함수
 colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
 
-modeBtn.addEventListener("click", onModeClick)
+modeBtn.addEventListener("click", onModeClick);
+destroyBtn.addEventListener("click", OnDestroyClick);
