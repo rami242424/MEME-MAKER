0396d4302ce47c98a0b3f7519e1c498a0b9eaf67
Rami <138556019+rami242424@users.noreply.github.com>
2023-12-19 12:49:28 +0900

    Fill 버튼 만들어 Fill하기

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// const colorOption = document.getElementsByClassName("color-option");
+const modeBtn = document.getElementById("mode-btn");
 const colorOptions = Array.from(
     document.getElementsByClassName("color-option")
 );
@@ -12,6 +12,7 @@ canvas.height = 800;
 ctx.lineWidth = lineWidth.value; 
 
 let isPainting = false; 
+let isFilling = false;
 
 function onMove(event) {
     if(isPainting){
@@ -44,21 +45,33 @@ function onColorChange(event){
 }
 
 function onColorClick(event){
-    // console.log(event.target);
-    // console.dir(event.target.dataset.color);
-    // ctx.strokeStyle = event.target.dataset.color;
-    // ctx.fillStyle = event.target.dataset.color;
-
     const colorValue = event.target.dataset.color;
     ctx.strokeStyle = colorValue;
     ctx.fillStyle = colorValue;
     color.value = colorValue;
 }
 
+function onModeClick(){
+    if (isFilling) {
+        isFilling = false;
+        modeBtn.innerText = "Fill";
+    } else {
+        isFilling = true;
+        modeBtn.innerText = "Draw";
+    }
+}
+
+function onCanvasClick(){
+    if(isFilling){
+        ctx.fillRect(0, 0, 800, 800);
+    }
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+canvas.addEventListener("click", onCanvasClick);
 
 lineWidth.addEventListener("change", onLineWidthChange);
 color.addEventListener("change", onColorChange);
@@ -66,3 +79,5 @@ color.addEventListener("change", onColorChange);
 
 // 컬러를 클릭할떄마다 호출 될 함수
 colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
+
+modeBtn.addEventListener("click", onModeClick)
