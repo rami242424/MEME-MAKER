3f70966bf502a6d12b94e4175944a5b81e4baeb8
Rami <138556019+rami242424@users.noreply.github.com>
2024-01-03 21:50:31 +0900

    Paths

--- app.js
+++ app.js
@@ -2,3 +2,13 @@ const canvas = document.querySelector("canvas");
 
 // context(ctx) = brush => 뭐든 그릴 수 있다.
 const ctx = canvas.getContext("2d");
+canvas.width = 800;
+canvas.height = 800;
+
+// 선만 그림 색 적용 X
+ctx.rect(50, 50, 100, 100);
+// 옵션 1 stroke - rect으로 그린 선에 색 적용
+// ctx.stroke();
+
+// 옵션 2 fill - rect으로 그린 선안에 색으로 채워 적용
+ctx.fill();
