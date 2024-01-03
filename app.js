adafda32c6ce6034cbd6194d4d3a77eabab682f7
Rami <138556019+rami242424@users.noreply.github.com>
2024-01-03 21:56:13 +0900

    moveTo and lineTo

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-// 선만 그림 색 적용 X
-ctx.rect(50, 50, 100, 100);
-// 옵션 1 stroke - rect으로 그린 선에 색 적용
-// ctx.stroke();
-
-// 옵션 2 fill - rect으로 그린 선안에 색으로 채워 적용
-ctx.fill();
+ctx.moveTo(50, 50); // 브러쉬 시작점
+ctx.lineTo(150, 50); // 기존 x:50>150, y:50>50 으로 줄을 긋다.
+ctx.fill(); // 선으로 채운다.
