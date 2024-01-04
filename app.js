c3be7c508a55bb1151e6f96f618923fb2d766cf6
Rami <138556019+rami242424@users.noreply.github.com>
2024-01-04 21:18:01 +0900

    집만들기

--- app.js
+++ app.js
@@ -5,11 +5,18 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.moveTo(50, 50);
-ctx.lineTo(150, 50);
-ctx.lineTo(150, 150);
-ctx.lineTo(50, 150);
-ctx.lineTo(50, 50);
+ctx.moveTo(200, 200);
+ctx.fillRect(200, 200, 50, 200);
+ctx.fillRect(400, 200, 50, 200);
+ctx.lineWidth = 3;
 
-ctx.stroke();
-// ctx.fill();
+// 문
+ctx.fillRect(300, 300, 50, 100);
+
+// 천장
+ctx.fillRect(200, 200, 200, 5);
+// 지붕
+ctx.moveTo(200, 200);
+ctx.lineTo(325, 100);
+ctx.lineTo(450, 200);
+ctx.fill();
