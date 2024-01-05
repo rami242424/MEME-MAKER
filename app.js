40cfc7b6291eb30724190331c3c6a57e5ab0d0ce
Rami <138556019+rami242424@users.noreply.github.com>
2024-01-05 20:15:47 +0900

    2.0 Painting Lines

--- app.js
+++ app.js
@@ -5,22 +5,8 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-// 양팔
-ctx.fillRect(215 - 40, 200 - 50, 15, 100);
-ctx.fillRect(350 - 40, 200 - 50, 15, 100);
+function onClick(){
+    
+}
 
-// 몸통
-ctx.fillRect(260 - 40, 200 - 50, 60, 200);
-
-// 머리
-ctx.arc(250, 100, 50, 0, 2 * Math.PI)
-ctx.fill();
-
-// 눈
-ctx.beginPath();
-ctx.fillStyle = "white";
-ctx.arc(270, 80, 5, 0, 2 * Math.PI);
-ctx.arc(230, 80, 5, 0, 2 * Math.PI);
-ctx.moveTo(250, 110);
-ctx.arc(250, 110, 10, 0, 1 * Math.PI);
-ctx.fill();
+canvas.addEventListener("click", onClick);
