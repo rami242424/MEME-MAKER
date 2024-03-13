1b8ac5b7ec6e269077044e9aae5c36f81a99d2cc
Rami <138556019+rami242424@users.noreply.github.com>
2024-03-13 21:58:32 +0900

    클릭으로 선그리기

--- app.js
+++ app.js
@@ -6,29 +6,12 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-// 1. 몸통과 팔
-ctx.fillRect(205, 200, 15, 100); // 팔1
-ctx.fillRect(350, 200, 15, 100); // 팔2
-ctx.fillRect(260, 200, 50, 200) // 몸통
+// 2.0 Painting Lines 클릭하는대로 선그어지게 만들기
 
-// 2. 머리
-ctx.arc(285, 150, 50, 0, 2 * Math.PI);
-ctx.fill();
+ctx.lineWidth = 2;
+function onClick(event){
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+}
 
-// 3. 눈
-ctx.beginPath();
-ctx.fillStyle = 'white';
-ctx.arc(270, 140, 7, 0, 2 * Math.PI);
-ctx.arc(300, 140, 7, 0, 2 * Math.PI);
-ctx.fill();
-
-// 4. 입
-ctx.beginPath();
-// ctx.strokeStyle = 'red';
-ctx.fillStyle = 'red';
-ctx.moveTo(270, 170);
-ctx.lineTo(300, 170);
-ctx.lineTo(285, 190);
-ctx.lineTo(270, 170);
-// ctx.stroke();
-ctx.fill();
+canvas.addEventListener("click", onClick);
