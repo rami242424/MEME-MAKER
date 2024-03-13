ff6393ed68b56dc5ae9b239886dfe4862a9aabf0
Rami <138556019+rami242424@users.noreply.github.com>
2024-03-13 19:59:49 +0900

    ctx.rect으로 선만 만들고, stroke로 선의 색깔을 넣어주기

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const canvas = document.querySelector("canvas");
 
-// context(ctx) = brush => 뭐든 그릴 수 있다.
+// context(ctx) = brush => 뭐든 그릴 수 있다. 그림을 그릴때 사용한다.
+// const context = canvas.getContext("2d");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
-
-function onClick(){
-    
-}
-
-canvas.addEventListener("click", onClick);
+// ctx.fillRect(50, 50, 100, 200);
+ctx.rect(50, 50, 100, 100); // 선만 만들고 색은 적용하지 않았음
+ctx.stroke();
