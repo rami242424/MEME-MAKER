bd5b1e4481d273a7218fdef62a0fdb3e2a3ccd79
Rami <138556019+rami242424@users.noreply.github.com>
2024-03-14 23:01:52 +0900

    그리는 그림판 완성

--- app.js
+++ app.js
@@ -6,42 +6,41 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-// 2.0 Painting Lines 클릭하는대로 선그어지게 만들기
-// 2-1 click > mousemove로 바꿔보기
-// 2-2 그어지는 선들 색깔 랜덤으로 다르게 만들기(배열이용)
-// 2-3 선마다 다른 선으로(랜덤) 그려지기, 그려질때마다 다른 색
-
-const color1 = [
-    '#55efc4',
-    '#00b894'
-];
-
-const color2 = [
-    '#ffeaa7',
-    '#fdcb6e'
-]
+// 2.1 Mouse Painting
+// moveTo는 우리가 선을 긋지 않으면서 브러쉬를 움직이게 해준다.
+// 2-1-1 유저가 마우스를 움직일 때 마다 moveTo를 호출함으로써 ???
+// 유저가 움직일 때, 연필을 움직여주기
+// 클릭하면, 클릭한 곳에서부터 선을 그려야함
+// => 우리는 moveTo를 해줘야한다, 왜냐하면 유저의 마우스가 있는 곳으로 브러쉬를 움직이고 싶기 떄문이다.
+// mousedown : 마우스를 누른채로 있는 것
 
 ctx.lineWidth = 2;
+let isPainting = false; // 마우스를 누르면 isPainting = true가 되어야한다.
+// 마우스가 눌렸을 때 어떤 일이 일어나야 할까?
+// 마우스가 눌렸을 때 유저가 그리고 싶어하는걸 그리게 해주자. => isPainting이라는 변수를 만들어주자.
+ctx.moveTo(200, 200);
+ctx.lineTo(400, 400);
+ctx.stroke();
 
-function MouseEvent1 (event){
-    ctx.beginPath();
-    ctx.moveTo(0, 0); // 클릭할때마다 moveTo(0,0)로 리셋해주기
-    const color = color1[Math.floor(Math.random() * color1.length)];
-    ctx.strokeStyle = color;
-    ctx.lineTo(event.offsetX, event.offsetY);
-    ctx.stroke();
+function onMove(event){
+    if(isPainting) {
+        ctx.lineTo(event.offsetX, event.offsetY)
+        ctx.stroke();
+        return;
+    }
+    ctx.moveTo(event.offsetX, event.offsetY);
 }
 
-function MouseEvent2 (event){
-    ctx.beginPath();
-    ctx.moveTo(800, 800); // 클릭할때마다 moveTo(0,0)로 리셋해주기
-    const color = color2[Math.floor(Math.random() * color2.length)];
-    ctx.strokeStyle = color;
-    ctx.lineTo(event.offsetX, event.offsetY);
-    ctx.stroke();
+function onMouseDown(){
+// 마우스가 눌렸을 때 유저가 그리고 싶어하는걸 그리게 해주기
+    isPainting = true
 }
 
-canvas.addEventListener("mousemove", MouseEvent1);
-canvas.addEventListener("mousemove", MouseEvent2);
+function onMouseUp(){
+    isPainting = false;
+}
 
+canvas.addEventListener('mousemove', onMove);
+canvas.addEventListener('mousedown', onMouseDown);
+canvas.addEventListener('mouseup', onMouseUp);
 
