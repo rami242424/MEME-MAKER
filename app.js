b889bf8aee3635ad5e1d4c56bb09440171eed266
Rami <138556019+rami242424@users.noreply.github.com>
2024-03-14 20:13:49 +0900

    마우스를 움직일떄 마다 선이 그어지고, 선의 색깔이 계속변한다

--- app.js
+++ app.js
@@ -7,11 +7,28 @@ canvas.width = 800;
 canvas.height = 800;
 
 // 2.0 Painting Lines 클릭하는대로 선그어지게 만들기
+// 2-1 click > mousemove로 바꿔보기
+// 2-2 그어지는 선들 색깔 랜덤으로 다르게 만들기(배열이용)
+
+const colors = [
+    "#ff3838",
+    "#ffb8b8",
+    "#c56cf0",
+    "#ff9f1a",
+    "#fff200",
+    "#32ff7e",
+    "#7efff5",
+];
 
 ctx.lineWidth = 2;
-function onClick(event){
+function MouseEvent (event){
+    ctx.moveTo(0, 0); // 클릭할때마다 moveTo(0,0)로 리셋해주기
+    const color = colors[Math.floor(Math.random() * colors.length)];
+    ctx.strokeStyle = color;
     ctx.lineTo(event.offsetX, event.offsetY);
     ctx.stroke();
 }
 
-canvas.addEventListener("click", onClick);
+canvas.addEventListener("mousemove", MouseEvent);
+
+
