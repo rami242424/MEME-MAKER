306c2515541f4e179b422e46f03605bedccb9510
Rami <138556019+rami242424@users.noreply.github.com>
2024-03-21 10:27:01 +0900

    event.target.value로 굵기 조절할때 해당값이 콘솔에 찍힘

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 
 // context(ctx) = brush => 뭐든 그릴 수 있다. 그림을 그릴때 사용한다.
@@ -6,18 +7,8 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-// 2.1 Mouse Painting
-// moveTo는 우리가 선을 긋지 않으면서 브러쉬를 움직이게 해준다.
-// 2-1-1 유저가 마우스를 움직일 때 마다 moveTo를 호출함으로써 ???
-// 유저가 움직일 때, 연필을 움직여주기
-// 클릭하면, 클릭한 곳에서부터 선을 그려야함
-// => 우리는 moveTo를 해줘야한다, 왜냐하면 유저의 마우스가 있는 곳으로 브러쉬를 움직이고 싶기 떄문이다.
-// mousedown : 마우스를 누른채로 있는 것
-
-ctx.lineWidth = 2;
-let isPainting = false; // 마우스를 누르면 isPainting = true가 되어야한다.
-// 마우스가 눌렸을 때 어떤 일이 일어나야 할까?
-// 마우스가 눌렸을 때 유저가 그리고 싶어하는걸 그리게 해주자. => isPainting이라는 변수를 만들어주자.
+ctx.lineWidth = lineWidth.value;
+let isPainting = false; 
 ctx.moveTo(200, 200);
 ctx.lineTo(400, 400);
 ctx.stroke();
@@ -40,8 +31,15 @@ function cancelPainting(){
     isPainting = false;
 }
 
+// 그리고 항상 event에 접근할 수 있다는 사실이 중요하다.
+// 왜냐면 event는 우리에게 input의 새로운 value(값)을 알려준다.
+function onLineWidthChange(event){
+    console.log(event.target.value);
+}
+
 canvas.addEventListener('mousemove', onMove);
 canvas.addEventListener('mousedown', startPainting);
 canvas.addEventListener('mouseup', cancelPainting);
 canvas.addEventListener('mouseleave', cancelPainting);
 
+lineWidth.addEventListener("change", onLineWidthChange);
